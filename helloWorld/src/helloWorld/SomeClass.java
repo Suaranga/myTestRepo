dc237f908fdf9b1a31eb2f6034a8169908e9bc24
Hemanthi <Hemanthi>
2016-03-28 14:05:11 +0100

    AS101 - Changed name to Jacky

--- helloWorld/src/helloWorld/SomeClass.java
+++ helloWorld/src/helloWorld/SomeClass.java
@@ -3,7 +3,7 @@ package helloWorld;
 public class SomeClass {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Jane");
+		System.out.println("My name is Jacky");
 
 	}
 
